9e0155a268581338aacd65aed6282e1e2e0d3e5d
Michael Jackson <michael@jackson.us>
2019-11-21 23:59:09 -0800

    Move comment

--- packages/react-router/jest-transformer.js
+++ packages/react-router/jest-transformer.js
@@ -6,9 +6,5 @@ function getBabelrcConfig(file) {
 }
 
 module.exports = babelJest.createTransformer(
-  // For some reason babel-jest is not able to find and use
-  // modules/.babelrc without this custom transformer. All
-  // we are doing here is reading in the configuration that
-  // it should have already found for us...
   getBabelrcConfig(__dirname + "/modules/.babelrc")
 );
--- packages/react-router/jest.config.js
+++ packages/react-router/jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
     __DEV__: true
   },
   testMatch: ["**/__tests__/**/*-test.js"],
+
+  // For some reason Jest is not able to find and use our
+  // Babel config in modules/.babelrc without this custom
+  // transform, but adding this fixes it... ?
   transform: {
     "\\.js$": "./jest-transformer.js"
   }
